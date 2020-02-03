79ef9a01c3c1c5b886cc1a6fc7773ddcba467c57
Stephanie <sorazemhr@gmail.com>
2020-02-03 11:44:38 -0300

    + material icon and card code

---
+++ projeto-2/src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="card">
+        <v-icon color="black">star</v-icon>
+        <img/>
+        <div class="container">
+            <h3></h3>
+            <p></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+
+    data() {
+        return {
+            //
+        }
+    }
+}
+</script>
