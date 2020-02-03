3548cc9e837cf794358a22286237d6f684f4ec57
Stephanie <sorazemhr@gmail.com>
2020-02-03 13:39:30 -0300

    + card and search bar

--- projeto-2/src/App.vue
+++ projeto-2/src/App.vue
@@ -5,10 +5,39 @@
       color="primary"
       dark
     >
-      <!-- Header -->
+      <v-toolbar-title>Beer Lover</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <p>Home</p>
+      <p>Favourites</p>
     </v-app-bar>
 
-    <v-content>
+    <v-content class="px-8">
+      <v-row class="my-12 d-flex justify-center align-center">
+          <v-text-field
+            solo
+            flat
+            outlined
+            clearable
+            hide-details
+            label="Pesquisar..."
+            v-model="itemSearch"
+            v-on:keyup.enter="searchGifs"
+            color="primary"
+            id="search"
+          >
+          </v-text-field>
+
+          <v-btn 
+            v-on:click="searchGifs" 
+            depressed 
+            color="primary"
+          >
+            <v-icon color="white">search</v-icon>
+          </v-btn>
+      </v-row>
+      
       <Card></Card>
     </v-content>
   </v-app>
@@ -25,7 +54,21 @@ export default {
   },
 
   data: () => ({
-    //
+    beers: []
   }),
 };
 </script>
+
+<style>
+  .v-input{
+    max-width: 70vw !important;
+  }
+
+  .v-btn{
+    height: 56px !important;
+  }
+
+  .v-btn, .v-text-field--outlined, .v-text-field--solo{
+    border-radius: 0 !important;
+  }
+</style>
--- projeto-2/src/components/Card.vue
+++ projeto-2/src/components/Card.vue
@@ -1,10 +1,12 @@
 <template>
-    <div id="card">
-        <v-icon color="black">star</v-icon>
-        <img/>
-        <div class="container">
-            <h3></h3>
-            <p></p>
+    <div id="card" class="elevation-2">
+        <v-icon id="favorite" color="black">star_border</v-icon>
+        <div class="px-4">
+            <img src=""/>
+            <div>
+                <h3>Uma header</h3>
+                <p>Texto texto texto texto</p>
+            </div>
         </div>
     </div>
 </template>
@@ -20,3 +22,18 @@ export default {
     }
 }
 </script>
+
+<style>
+    #card {
+        display: flex;
+        flex-direction: column;
+        max-width: 30vw;
+        position: relative;
+    }
+
+    #favorite{
+        position: absolute;
+        top: 8px;
+        right: 8px;
+    }
+</style>
